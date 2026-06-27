fa00f6ce581d0d0ac13db389b3fe88ca0699c319
dev-ebimo <shadrackebimotimi@gmail.com>
2026-06-27 22:04:15 +0100

    Fix CORS security whitelist and verify API url paths

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:5500', 
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
+    'https://kings-casino-backend.onrender.com' // ◄ ADD YOUR LIVE RENDER URL HERE
 ];
 
 app.use(cors({
